67e628f0db20e13f2b654c793950f7c13df394d0
Push Tech <Pushtechja@gmail.com>
2021-11-18 18:24:15 -0500

    basic application layout.
    basic application layout.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import {Button, Menu, Typography, Avatar} from 'antd';
+import { Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import icon from '../images/Cryptjamlogo.png'
 
 const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar />
+                <Avatar src={icon} size="large"/>
                 <Typography.Title level={2} className="logo">
-                    <link to="/">cryptjam</link>
+                    <Link to="/">cryptjam</Link>
                 </Typography.Title>
-                <button className="menu-control-container">
-
-                </button>
 
             </div>
+            <Menu theme="dark">
+                <Menu.item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.item>
+                <Menu.item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.item>
+                <Menu.item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.item>
+                <Menu.item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.item>
+               
+            </Menu>
             
         </div>
     )
